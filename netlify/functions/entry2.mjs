f2d328efc04c78808a3107465acd2cddedf41639
thepassle <pascalschilp@gmail.com>
2022-05-05 08:28:07 +0200

    chore: exclude build output

--- netlify/functions/entry2.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page0, d as _page1, e as _page2 } from './chunks/chunk.632ca3bc.mjs';
-
-const pageMap = new Map([['src/pages/index.astro', _page0],['src/pages/networkonly-endpoint.js', _page1],['src/pages/foo.js', _page2],]);
-const renderers = [];
-
-export { pageMap, renderers };
